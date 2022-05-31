ef905033a667c24bb690c7c08927822de94ad0c2
WIMWHAG <william.johnrivera@yahoo.com>
2022-05-31 17:01:35 +0000

    asserEqual

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
