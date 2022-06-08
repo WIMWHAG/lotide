3fc580fc3fb7113a4eab41e45864f23137040c35
WIMWHAG <william.johnrivera@yahoo.com>
2022-06-08 22:30:50 +0000

    findKey

---
+++ findKey.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢Assertion Passed🟢: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed❌: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(obj, callback) {
+  for (const key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3)); // => "noma"
