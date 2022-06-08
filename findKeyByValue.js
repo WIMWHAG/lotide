19af144cea8533afe4fb44532c545835984a0a01
WIMWHAG <william.johnrivera@yahoo.com>
2022-06-08 10:48:51 +0000

    findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢Assertion Passed🟢: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed❌: ${actual} !== ${expected}`);
+  }
+};
+
+findKeyByValue = function(object, value) {
+  let keys = Object.keys(object);
+  for (let key of keys) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+
+//test code
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
