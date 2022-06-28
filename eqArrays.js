a7d7b889094653f59ecf9ed74e0cbcb9d58189e7
WIMWHAG <william.johnrivera@yahoo.com>
2022-06-28 23:26:00 +0000

    eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🟢Assertion Passed🟢: ${actual} === ${expected}`);
+    console.log(`🟢: ${actual} === ${expected}`);
   } else {
-    console.log(`❌Assertion Failed❌: ${actual} !== ${expected}`);
+    console.log(`❌: ${actual} !== ${expected}`);
   }
-}; 
+};
 
 const eqArrays = function(actual, expected) {
   if (actual.length !== expected.length)
@@ -16,3 +16,16 @@ for (let i = 0; i < actual.length; i++) {
  }
  return true;
 }
+
+
+// Test Codes:
+// eqArrays([1, 2, 3], [1, 2, 3]) // => true
+// eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
